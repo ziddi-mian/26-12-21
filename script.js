4d2f581728ae78eba1e5d30fc319259785582cc9
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-26 11:47:26 +0500

    js

--- script.js
+++ script.js
@@ -1222,3 +1222,18 @@ function startOfMonth(date)
 dt = new Date(); 
 
 console.log(startOfMonth(dt).toString());
+
+
+
+// Write a JavaScript function to get the month end date.  
+
+function endOfMonth(date)
+  {
+     
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0);
+ 
+  }
+
+dt = new Date(); 
+
+console.log(endOfMonth(dt).toString());
