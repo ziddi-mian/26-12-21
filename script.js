e9d3b21cb5304ada675ea7d7fdcc940962ba1007
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-26 11:18:16 +0500

    js

--- script.js
+++ script.js
@@ -23,3 +23,29 @@ var is_date = function(input) {
   console.log(is_date(new Date(86400000)));
   console.log(is_date(new Date(99,5,24,11,33,30,0)));
   console.log(is_date([1, 2, 4, 0]));
+
+
+  // program 2
+
+//   Write a JavaScript function to get the current date.   
+
+// Note : Pass a separator as an argument.
+// Test Data :
+// console.log(curday('/'));
+// console.log(curday('-'));
+// Output :
+// "11/13/2014"
+// "11-13-2014"
+
+var curday = function(ap){
+    today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    
+    if(dd<10) dd='0'+dd;
+    if(mm<10) mm='0'+mm;
+    return (mm+ap+dd+ap+yyyy);
+    };
+    console.log(curday('/'));
+    console.log(curday('-'));
